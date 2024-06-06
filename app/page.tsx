40d7c436700ff035323e9ba09859425659797752
Pranav Deshmukh <pranavdeshmukh190@gmail.com>
2024-06-06 20:07:15 +0530

    feat:canvas, working pencil, colors added

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,50 @@
-import { FC } from "react";
+'use client'
 
-interface HomeProps {
+import { FC, useState } from 'react'
+import { ChromePicker } from 'react-color'
+import { useDraw } from './hooks/useDraw'
+
+const Page: FC = () => {
   
-}
- 
-const Home: FC<HomeProps> = () => {
-  return ( 
-    <div className="w-screen h-screen">
-      <canvas 
-      className="w-[100%] h-[100%] border border-black rounded-md"/>
+  const [color, setColor] = useState<string>('#000')
+  const { canvasRef, onMouseDown, clear } = useDraw(drawLine)
+
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = currentPoint
+    const lineColor = color
+    const lineWidth = 5
+
+    let startPoint = prevPoint ?? currentPoint
+    ctx.beginPath()
+    ctx.lineWidth = lineWidth
+    ctx.strokeStyle = lineColor
+    ctx.moveTo(startPoint.x, startPoint.y)
+    ctx.lineTo(currX, currY)
+    ctx.stroke()
+
+    ctx.fillStyle = lineColor
+    ctx.beginPath()
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI)
+    ctx.fill()
+  }
+
+  return (
+    <div className='w-screen h-screen bg-white relative'>
+      <div className='absolute top-10 left-10 flex flex-col gap-10 pr-10'>
+        <ChromePicker color={color} onChange={(color) => setColor(color.hex)} />
+        <button type='button' className='p-2 rounded-md border border-black' onClick={clear}>
+          Clear canvas
+        </button>
+      </div>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        className='border border-black rounded-md w-full h-full'
+      />
     </div>
-   );
+  )
 }
- 
-export default Home;
+
+export default Page
